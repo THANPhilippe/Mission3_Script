5d930cc34b5e38f6b8aa9e2f6131ac8f517b62ba
Nhân THAN <philippethan@hotmail.com>
2016-11-28 15:12:56 +0100

    Tache 1

--- PPE_Mission_3/PPE_Mission_3/ConnexionSql.cs
+++ PPE_Mission_3/PPE_Mission_3/ConnexionSql.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.Sql;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,46 @@ namespace PPE_Mission_3
 {
     class ConnexionSql
     {
-        
+        private static MySqlConnection SqlCo = null;
+
+
+
+        //Constructor
+        private ConnexionSql(string unProvider)
+        {
+            string connectionString;
+            connectionString = unProvider;
+
+            SqlCo = new MySqlConnection(connectionString);
+        }
+
+
+        public static MySqlConnection getInstance(string unProvider)
+        {
+            if (null == SqlCo)
+            { // Premier appel
+              //  ConnexionSql.Initialize(unProvider, uneDataBase);
+
+                new ConnexionSql(unProvider);
+
+            }
+            return SqlCo;
+        }
+
+
+
+
+        //open connection to database
+        public static void OpenConnection()
+        {
+            SqlCo.Open();
+        }
+
+        //Close connection
+        public static void CloseConnection()
+        {
+            SqlCo.Close();
+        }
+
     }
 }
--- PPE_Mission_3/PPE_Mission_3/Form1.cs
+++ PPE_Mission_3/PPE_Mission_3/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,24 @@ namespace PPE_Mission_3
 {
     public partial class Form1 : Form
     {
+        private MySqlDataReader SqlRea;
+        private MySqlConnection SqlCo = ConnexionSql.getInstance("SERVER = 127.0.0.1; DATABASE=pthan;UID=root;PASSWORD=");
+
         public Form1()
         {
             InitializeComponent();
+            afficherDonnées();
+        }
+
+        public void afficherDonnées()
+        {
+
+            // Ouverture de la connexion
+            SqlCo.Open();
+
+            // Instanciation de l’objet Command
+            MySqlCommand SqlCom = new MySqlCommand("Select * from Employé", SqlCo);
+
         }
     }
 }
